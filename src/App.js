cb9cfaabbe18b3e845483fb7906b9dc65e430b90
Anderson Pereira <andersonfelipeafspjf@gmail.com>
2023-02-16 07:18:28 -0300

    change main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meu exemplo react
+          Meu exemplo react actions 
         </p>
         <a
           className="App-link"
